5f8a20fe86b64944e28a69574e372412309e3027
Thanh Vu <vuvanthanh.haui.ktpm1@gmail.com>
2018-11-06 17:02:31 +0700

    Initial Commit

---
+++ FuckingEnglish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FuckingEnglish
+//
+//  Created by fuxsociety on 11/6/18.
+//  Copyright © 2018 fuxsociety. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FuckingEnglish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FuckingEnglish
+//
+//  Created by fuxsociety on 11/6/18.
+//  Copyright © 2018 fuxsociety. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
